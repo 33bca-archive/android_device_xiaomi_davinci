eccc8f3ca1b4265de7edd3b3ad64dd529c3ac6d1
TheScarastic <warabhishek@gmail.com>
2016-12-02 02:48:41 -0500

    davinci: Add assertions on modem version
     * Based off android_device_xiaomi_msm8996-common
    
    Change-Id: I4db528dd565cc21d7cbd590de54d1f400d2de223

--- releasetools.py
+++ releasetools.py
@@ -1,5 +1,4 @@
 # Copyright (C) 2009 The Android Open Source Project
-# Copyright (C) 2019 The Mokee Open Source Project
 # Copyright (C) 2019 The LineageOS Open Source Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -14,11 +13,20 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import hashlib
 import common
 import re
 
+def FullOTA_Assertions(info):
+  AddModemAssertion(info, info.input_zip)
+  return
+
 def FullOTA_InstallEnd(info):
   OTA_InstallEnd(info)
+  return
+
+def IncrementalOTA_Assertions(info):
+  AddModemAssertion(info, info.target_zip)
   return
 
 def IncrementalOTA_InstallEnd(info):
@@ -30,6 +38,17 @@ def AddImage(info, basename, dest):
   data = info.input_zip.read("IMAGES/" + basename)
   common.ZipWriteStr(info.output_zip, name, data)
   info.script.AppendExtra('package_extract_file("%s", "%s");' % (name, dest))
+
+def AddModemAssertion(info, input_zip):
+  android_info = info.input_zip.read("OTA/android-info.txt")
+  m = re.search(r'require\s+version-modem\s*=\s*(.+)', android_info)
+  if m:
+    timestamp, firmware_version = m.group(1).rstrip().split(',')
+    if ((len(timestamp) and '*' not in timestamp) and \
+        (len(firmware_version) and '*' not in firmware_version)):
+      cmd = 'assert(xiaomi.verify_modem("{}") == "1" || abort("ERROR: This package requires firmware from MIUI {} global stable build or newer. Please upgrade firmware and retry!"););'
+      info.script.AppendExtra(cmd.format(timestamp, firmware_version))
+  return
 
 def OTA_InstallEnd(info):
   info.script.Print("Patching firmware images...")
