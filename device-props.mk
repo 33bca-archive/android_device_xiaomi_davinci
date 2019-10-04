a699006288568c50cef6f060fc6248c502b9f8ce
Jake Weinstein <jake@aospa.co>
2019-10-04 15:34:55 +0200

    davinci: Force triple buffering in SF
     * Matches Qualcomm configuration
    
    Change-Id: I24f03750bda3c290c704b0363fc6b77139b9fe9b

--- device-props.mk
+++ device-props.mk
@@ -45,7 +45,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     debug.sf.disable_backpressure=1 \
     debug.sf.enable_gl_backpressure=1 \
-    debug.sf.enable_hwc_vds=1
+    debug.sf.enable_hwc_vds=1 \
+    ro.surface_flinger.max_frame_buffer_acquired_buffers=3
 
 # IMS
 PRODUCT_PROPERTY_OVERRIDES += \
